1cf3f612a0b726f946777e291973f47043c32212
TeresaNesteby <teresa.nesteby@gmail.com>
2015-08-25 00:42:49 -0700

    adjusts css for background and text alignment (center)

--- app.js
+++ app.js
@@ -2,8 +2,17 @@
 // If an ng-app directive is found, AngularJS will load any module named in the directive, and compile the DOM with ng-app as the root of the application.
 var app = angular.module("myChatTracker", []);
 
-app.controller("myNoteControl", function() {
-
+app.controller("myNoteController", function() {
+    var list = {};
+    // hardcoded list
+    $scope.notes = list;
+    // add note
+    $scope.add = function(note) {
+      list.push(note);
+    };
+    $scope.show = function(){
+      // display all notes
+    }
     $scope.clear = function() {$scope.message = "";};
     $scope.save  = function() {alert("Note Saved");};
 });
@@ -17,3 +26,19 @@ app.controller('formCtrl', function($scope) {
     };
     $scope.reset();
 });
+
+// -------------------To add Chart
+// // angular.module("app", ["chart.js"]).controller("PolarAreaCtrl", function ($scope) {
+//   $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"];
+//   $scope.data = [300, 500, 100, 40, 120];
+// });
+
+// Settings
+// .chart-polar-area
+// data: series data
+// labels: series labels
+// legend (default: false): show legend below the chart
+// options (default: {}): Chart.js options
+// click (optional): onclick event handler
+// hover (optional): onmousemove event handler
+// colours (default to global colours): colours for the chart
